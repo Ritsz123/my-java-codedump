8159fb53fcb27311ade439f33bfcabdfb52bfc70
ritesh <riteshkhadse12@gmail.com>
2021-03-20 12:42:03 +0530

    2nd largest element in array

--- src/gfgSelfPaced/ArraysPractice.java
+++ src/gfgSelfPaced/ArraysPractice.java
@@ -239,7 +239,24 @@ class ArraysPractice {
 		}
 		return profit;
 	}
-	
+
+	static int secondLargestElementInArray(int[] arr){
+		int n = arr.length;
+		int res = -1, largest = 0;
+
+		for (int i=1;i<n;i++){
+			if(arr[i]>arr[largest]){
+				res = largest;
+				largest = i;
+			}else if(arr[i]!=arr[largest]){
+				if (res == -1 || arr[i] > arr[res]){
+					res = i;
+				}
+			}
+		}
+		return arr[res];
+	}
+
 	static int binarySearch(int [] arr,int left,int right,int target) {
 		if(left<=right) {
 			int mid = (left + right)/2;
