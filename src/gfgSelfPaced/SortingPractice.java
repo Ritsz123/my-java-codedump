f12288e16eea868581210835a2d5799c3f28004a
ritesh <riteshkhadse12@gmail.com>
2021-04-14 12:30:05 +0530

    add. merge without extra space

--- src/gfgSelfPaced/SortingPractice.java
+++ src/gfgSelfPaced/SortingPractice.java
@@ -76,7 +76,27 @@ public class SortingPractice {
 		System.out.println(meetMaximumGuestsInTheParty(arr, dept));
 	}
 	
-	
+
+	static void mergeWithoutExtraSpace(int[] a,int[] b){
+		int n = a.length;
+		int m = b.length;
+
+		int i = n-1;
+		int j = 0;
+
+		while(i >= 0 && j < m){
+			if(a[i] <= b[j]){
+				i--;
+			}else{
+				//swap
+				int temp = a[i];
+				a[i] = b[j];
+				b[j] = temp;
+				j++;
+			}
+		}
+	}
+
 	static int meetMaximumGuestsInTheParty(int [] arr,int [] dept) {
 		int n = arr.length;
 //		Optimal O(n log n) 
