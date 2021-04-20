2722ae04b1481a9d6118e4f81a0b6b42d64fda40
ritesh <riteshkhadse12@gmail.com>
2021-04-20 12:45:13 +0530

    longest common substring

--- src/adityaVermaDP/LongestCommonSubSequence.java
+++ src/adityaVermaDP/LongestCommonSubSequence.java
@@ -2,7 +2,7 @@ package adityaVermaDP;
 
 public class LongestCommonSubSequence {
     public static void main(String[] args) {
-        String s1 = "shiter";
+        String s1 = "sietmr";
         String s2 = "ritesh";
 
 //        System.out.println(longestCommonSubSequenceRecursive(s1,s2,s1.length(),s2.length()));
@@ -15,7 +15,9 @@ public class LongestCommonSubSequence {
 //        }
 //        System.out.println(lcsMemoized(s1,s2,s1.length(),s2.length(),dp));
 
-        System.out.println(lcsTD(s1,s2,s1.length(),s2.length()));
+//        System.out.println(lcsTD(s1,s2,s1.length(),s2.length()));
+
+        System.out.println(longestCommonSubstring(s1,s2,s1.length(),s2.length()));
     }
 
     static int longestCommonSubSequenceRecursive(String s1,String s2,int n,int m){
@@ -43,8 +45,8 @@ public class LongestCommonSubSequence {
     static  int lcsTD(String s1,String s2,int n,int m){
         int[][] dp = new int[n+1][m+1];
         //initialization
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=m;j++){
                 if (i==0 || j==0){
                     dp[i][j] = 0;
                 }
@@ -60,5 +62,28 @@ public class LongestCommonSubSequence {
             }
         }
         return dp[n][m];
+    }
+
+    static int longestCommonSubstring(String s1,String s2,int n,int m){
+        int[][] dp = new int[n+1][m+1];
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=m;j++){
+                if (i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        int mx = 0;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if (s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 0;
+                }
+                mx = Math.max(mx,dp[i][j]);
+            }
+        }
+        return mx;
     }
 }
