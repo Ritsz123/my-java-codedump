7efee083e5f304f706d6c73ed7a167b7ce717fcc
ritesh <riteshkhadse12@gmail.com>
2021-05-25 12:42:26 +0530

    fix. print shortest common SuperSequence

--- src/adityaVermaDP/LongestCommonSubSequence.java
+++ src/adityaVermaDP/LongestCommonSubSequence.java
@@ -148,7 +148,9 @@ public class LongestCommonSubSequence {
         for (int i=1;i<=s1.length();i++){
             for (int j = 1;j<=s2.length();j++){
                 if (s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]) + 1;
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    i--;
+                    j--;
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
