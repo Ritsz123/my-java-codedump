a1432e0d523f88efc592a57dca1a57c15c98500a
ritesh <riteshkhadse12@gmail.com>
2021-05-28 16:50:26 +0530

    n Queen clean code

--- src/dsaone/backtracking/NQueenProblem.java
+++ src/dsaone/backtracking/NQueenProblem.java
@@ -1,5 +1,7 @@
 package dsaone.backtracking;
 
+import java.util.ArrayList;
+
 public class NQueenProblem {
 
     boolean isSafe(int [][] arr, int x,int y,int n){
@@ -55,14 +57,86 @@ public class NQueenProblem {
         }
         return false;
     }
+
+    void nQueenSolve(boolean [][] arr, int col, ArrayList<ArrayList<Integer>> ans){
+        int n = arr.length;
+
+        if (col == n){
+            ArrayList<Integer> curr = new ArrayList<>();
+            for (int i = 0;i<n;i++){
+                for (int j = 0;j<n;j++){
+                    if (arr[i][j]){
+                        curr.add(j+1);
+                        break;
+                    }
+                }
+            }
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int row = 0; row<n; row++){
+
+            if(isSafeHere(arr,row,col)){
+                arr[row][col] = true;
+
+                nQueenSolve(arr,col+1,ans);
+
+                arr[row][col] = false;
+            }
+        }
+    }
+
+    private boolean isSafeHere(boolean[][] arr, int row, int col) {
+        int tRow = row;
+        int tCol = col;
+
+        // left diagonal top
+        while(row >= 0 && col >= 0){
+            if (arr[row][col]){
+                return false;
+            }
+            row--;
+            col--;
+        }
+
+        row = tRow;
+        col = tCol;
+        // only left
+        while(col >= 0){
+            if (arr[row][col]){
+                return false;
+            }
+            col--;
+        }
+
+        col = tCol;
+        //left diagonal bottom
+        while (row< arr.length && col >= 0){
+            if (arr[row][col]){
+                return false;
+            }
+            row++;
+            col--;
+        }
+
+        return true;
+    }
 }
 
 class Runner{
     public static void main(String[] args) {
         NQueenProblem nq = new NQueenProblem();
         int n = 4;
-        int [][] arr = new int[n][n];
-        boolean ans = nq.nQueen(arr,0,n);
-        System.out.println(ans);
+        boolean [][] arr = new boolean[n][n];
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        nq.nQueenSolve(arr,0,ans);
+
+        for (int i =0;i<ans.size();i++){
+            for (Integer x: ans.get(i)){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
     }
 }
