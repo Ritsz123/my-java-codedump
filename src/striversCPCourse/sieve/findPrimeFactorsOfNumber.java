3a3d498dfe1b5b04b35953f20063c59fd845afef
ritesh <riteshkhadse12@gmail.com>
2021-07-07 16:31:10 +0530

    find prime factors of a number

---
+++ src/striversCPCourse/sieve/findPrimeFactorsOfNumber.java
@@ -0,0 +1,34 @@
+package striversCPCourse.sieve;
+
+public class findPrimeFactorsOfNumber {
+    int max = (int) 1e6;
+    int[] sieve = new int[max];
+
+    void sieve() {
+        for(int i = 2; i<=max;i++){
+            sieve[i] = i;
+        }
+        for (int i = 2; i <= max;i++){
+//            check if sieve[i] is prime
+            if (sieve[i] == i){
+                for (int j = i * i; j<= max;j += i){
+                    if (sieve[j] == j){
+                        //mark it with minimum prime factor
+                        sieve[j] = i;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+
+        for (int i = 2; i<=n;i++){
+            while (n % i == 0){
+                System.out.println(i);
+                n = n/i;
+            }
+        }
+    }
+}
