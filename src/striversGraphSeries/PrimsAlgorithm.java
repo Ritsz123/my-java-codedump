98a99660c982be2425ed2e27cfeafafb758d1a5f
ritesh <riteshkhadse12@gmail.com>
2021-07-09 15:31:36 +0530

    refactor prim's algo

--- src/striversGraphSeries/PrimsAlgorithm.java
+++ src/striversGraphSeries/PrimsAlgorithm.java
@@ -15,37 +15,37 @@ public class PrimsAlgorithm {
     }
 
     static void primsAlgorithm(ArrayList<ArrayList<Pair>> adj, int n){
-        int[] dist = new int[n];
-        boolean[] mstSet = new boolean[n];
+        int[] weights = new int[n];
+        boolean[] visited = new boolean[n];
         int[] parent = new int[n];
 
         for (int i = 0;i<n;i++){
-            dist[i] = Integer.MAX_VALUE;
+            weights[i] = Integer.MAX_VALUE;
         }
 
-        dist[0] = 0;
+        weights[0] = 0;
 
         //as we will only have n-1 edges in mst therefore only traverse till n-1
         for(int i = 0;i<n-1;i++){
             int min = Integer.MAX_VALUE, u = 0;
 
-            //find minimum weight such that it is not used
+            //pick vertex with minimum weight such that is not visited
             //greedy strategy....
             for (int v = 0; v<n;v++) {
-                if (mstSet[v] == false && dist[v] < min){
-                    min = dist[v];
+                if (visited[v] == false && weights[v] < min){
+                    min = weights[v];
                     u = v;
                 }
             }
 
             //mark the selected weight as used
-            mstSet[u] = true;
+            visited[u] = true;
 
             // for all adjacent nodes if current weight is less than selected weight then selected weight = current weight
             for (Pair x: adj.get(i)) {
-                if (mstSet[x.v] == false && x.weight < dist[x.v]){
+                if (visited[x.v] == false && x.weight < weights[x.v]){
                     parent[x.v] = u;
-                    dist[x.v] = x.weight;
+                    weights[x.v] = x.weight;
                 }
             }
         }
